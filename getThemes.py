d4a89aad90789ac5e695e0152a5e7645880ca40f
PeppawouldusayBUNANA <ansh_tiwari2808@outlook.com>
2025-06-20 18:04:56 +0530

    Edit getThemes to get theme and get lastUsed_theme

--- getThemes.py
+++ getThemes.py
@@ -2,7 +2,7 @@
 # Gets the themes as the user suggests.
 import json
 
-def load_theme(theme_name: str, file_path="themes.json") -> dict:
+def load_theme(theme_name: str, file_path="themes.json", last_used_file="lastUsed_theme.json") -> dict:
     """
     Loads the specified theme from a JSON file.
 
@@ -18,7 +18,16 @@ def load_theme(theme_name: str, file_path="themes.json") -> dict:
     
     theme = all_themes.get(theme_name)
     
+    with open(last_used_file, "w") as file:
+        json.dump({"lastUsedTheme": theme_name}, file)
+
     if theme is None:
         return None
     
     return theme
+
+
+def load_last_theme(file_path = "themes.json") -> dict:
+    """
+    Loads the theme last used from the json file
+    """
